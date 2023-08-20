b6f28547101b8430e3bd93cd2bbde84fb652cd45
LakiG <garg.lakshya@gmail.com>
2023-08-20 17:40:33 +0530

    feat: update constants

--- constants/constants.py
+++ constants/constants.py
@@ -1,22 +1,25 @@
-# Notes
+"""Module compliance-tests-ga4gh-tes.constants.constants.py
+
+This module contains the constant values specific to TES
+"""
+
+# API Constants
 # 1. Basic & Full views have same required fields. Hence, validating Basic views against Full view Model.
 
-SERVICE_CONSTANTS = {
-    "DIR_NAME": "compliance-tests-ga4gh-tes",
-    "REQUEST_HEADERS": {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    "ENDPOINT_TO_MODEL": {
-        'service_info': 'TesServiceInfo',
-        'list_tasks_MINIMAL': 'TesListTasksResponseMinimal',
-        'list_tasks_BASIC': 'TesListTasksResponse',
-        'list_tasks_FULL': 'TesListTasksResponse',
-        'get_task_MINIMAL': 'TesTaskMinimal',
-        'get_task_BASIC': 'TesTask',
-        'get_task_FULL': 'TesTask',
-        'create_task': 'TesCreateTaskResponse',
-        'create_task_request_body': 'TesTask',
-        'cancel_task': 'TesCancelTaskResponse'
-    }
+ENDPOINT_TO_MODEL = {
+    'service_info': 'TesServiceInfo',
+    'list_tasks_MINIMAL': 'TesListTasksResponseMinimal',
+    'list_tasks_BASIC': 'TesListTasksResponse',
+    'list_tasks_FULL': 'TesListTasksResponse',
+    'get_task_MINIMAL': 'TesTaskMinimal',
+    'get_task_BASIC': 'TesTask',
+    'get_task_FULL': 'TesTask',
+    'create_task': 'TesCreateTaskResponse',
+    'create_task_request_body': 'TesTask',
+    'cancel_task': 'TesCancelTaskResponse'
+}
+
+REQUEST_HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
 }
