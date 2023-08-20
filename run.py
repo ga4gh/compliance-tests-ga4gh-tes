609d548b3e194495373ec6d5b70430cdbae2397a
LakiG <garg.lakshya@gmail.com>
2023-08-20 17:41:49 +0530

    feat: add entry point

---
+++ run.py
@@ -0,0 +1,9 @@
+"""Module compliance-tests-ga4gh-tes.run.py
+
+This module contains the main function responsible for triggering the compliance tests
+"""
+
+from compliance_suite import cli
+
+if __name__ == "__main__":
+    cli.report()
