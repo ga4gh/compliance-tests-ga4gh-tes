9d7007075808dfae24562c33b5b1e4ea801d100d
LakiG <garg.lakshya@gmail.com>
2023-08-27 16:26:35 +0530

    feat: remove code

--- run.py
+++
@@ -1,9 +0,0 @@
-"""Module compliance-tests-ga4gh-tes.run.py
-
-This module contains the main function responsible for triggering the compliance tests
-"""
-
-from compliance_suite import cli
-
-if __name__ == "__main__":
-    cli.report()
